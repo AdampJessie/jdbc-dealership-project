8e3641925736e6a8826e9c7aa1d3d364c8cce082
unknown <adampjessie@gmail.com>
2025-06-17 22:56:35 -0400

    Updated VehicleDAO to fix logic errors, finished ContractDAO

--- src/main/java/com/pluralsight/dealership/DataManager/ContractDAO.java
+++ src/main/java/com/pluralsight/dealership/DataManager/ContractDAO.java
@@ -1,6 +1,13 @@
 package com.pluralsight.dealership.DataManager;
 
+import com.pluralsight.dealership.Models.Contract;
+import com.pluralsight.dealership.Models.LeaseContract;
+import com.pluralsight.dealership.Models.SalesContract;
+import com.pluralsight.dealership.Models.Vehicle;
 import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 public class ContractDAO {
 
@@ -10,5 +17,61 @@ public class ContractDAO {
     public ContractDAO(BasicDataSource dataSource) {
         this.dataSource = dataSource;
     }
+
+    public boolean addLeaseContract(LeaseContract contract){
+
+        boolean success = false;
+        Vehicle contractVehicle = contract.getVehicleSold();
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     """
+                             INSERT INTO `lease_contracts`
+                             (`VIN`,`lease_start`,
+                             `lease_end`,`monthly_payment`)
+                             VALUES
+                             (?, ?, ?, ?);
+                             """)) {
+            preparedStatement.setInt(1, contractVehicle.getVin());
+            preparedStatement.setString(2, contract.getDate());
+            preparedStatement.setString(3, contract.getEndDate());
+            preparedStatement.setDouble(4, contract.getMonthlyPayment());
+
+            success = (preparedStatement.executeUpdate() != 0);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return success;
+    }
+
+    public boolean addSalesContract(SalesContract contract){
+
+        boolean success = false;
+        Vehicle contractVehicle = contract.getVehicleSold();
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     """
+                             INSERT INTO `sales_contracts`
+                             (`VIN`,`sale_date`,`monthly_payment`)
+                             VALUES
+                             (?, ?, ?);
+                             """)) {
+            preparedStatement.setInt(1, contractVehicle.getVin());
+            preparedStatement.setString(2, contract.getDate());
+            preparedStatement.setDouble(3, contract.getMonthlyPayment());
+
+            success = (preparedStatement.executeUpdate() != 0);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return success;
+    }
+
+
 
 }
--- src/main/java/com/pluralsight/dealership/DataManager/VehicleDAO.java
+++ src/main/java/com/pluralsight/dealership/DataManager/VehicleDAO.java
@@ -26,11 +26,11 @@ public class VehicleDAO {
         try (Connection connection = dataSource.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(
                      """
-                             insert into vehicles 
+                             insert into vehicles
                              (`VIN`,`make`,`model`,
                               `vehicle_year`,`SOLD`,
                               `color`,`vehicle_type`,
-                              `odometer`,`price`) 
+                              `odometer`,`price`)
                              VALUES
                               (?, ?, ?, ?, 0, ?, ?, ?, ?)
                              """)) {
@@ -50,6 +50,211 @@ public class VehicleDAO {
         }
 
         return success;
+    }
+
+    public List<Vehicle> getVehiclesByPrice(double minPrice, double maxPrice){
+        List<Vehicle> vehicles = new ArrayList<>();
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                     "SELECT * FROM dealership.vehicles " +
+                             "WHERE price >= ? AND price <= ?;")) {
+
+            statement.setDouble(1, minPrice);
+            statement.setDouble(2, maxPrice);
+
+            try (ResultSet results = statement.executeQuery()) {
+                while (results.next()) {
+                    int vin = results.getInt("VIN");
+                    int vehicleYear = results.getInt("vehicle_year");
+                    String make = results.getString("make");
+                    String model = results.getString("model");
+                    String vehicleType = results.getString("vehicle_type");
+                    String color = results.getString("color");
+                    int odometer = results.getInt("odometer");
+                    double price = results.getDouble("price");
+
+
+                    Vehicle vehicle = new Vehicle(vin, vehicleYear, make, model, vehicleType, color, odometer, price);
+                    vehicles.add(vehicle);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        return vehicles;
+    }
+
+    public List<Vehicle> getVehiclesByMakeModel(String searchMake, String searchModel){
+        List<Vehicle> vehicles = new ArrayList<>();
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                     "SELECT * FROM vehicles " +
+                             "WHERE make LIKE ? AND model LIKE ?")) {
+            statement.setString(1, searchMake);
+            statement.setString(2, searchModel);
+
+            try (ResultSet results = statement.executeQuery()) {
+                while (results.next()) {
+                    int vin = results.getInt("VIN");
+                    int vehicleYear = results.getInt("vehicle_year");
+                    String make = results.getString("make");
+                    String model = results.getString("model");
+                    String vehicleType = results.getString("vehicle_type");
+                    String color = results.getString("color");
+                    int odometer = results.getInt("odometer");
+                    double price = results.getDouble("price");
+
+
+                    Vehicle vehicle = new Vehicle(vin, vehicleYear, make, model, vehicleType, color, odometer, price);
+                    vehicles.add(vehicle);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        return vehicles;
+    }
+
+    public List<Vehicle> getVehiclesByYear(int minYear, int maxYear){
+        List<Vehicle> vehicles = new ArrayList<>();
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                     "SELECT * FROM vehicles " +
+                             "WHERE vehicle_year >= ? AND vehicle_year <= ?")) {
+            statement.setInt(1, minYear);
+            statement.setInt(2, maxYear);
+
+            try (ResultSet results = statement.executeQuery()) {
+                while (results.next()) {
+                    int vin = results.getInt("VIN");
+                    int vehicleYear = results.getInt("vehicle_year");
+                    String make = results.getString("make");
+                    String model = results.getString("model");
+                    String vehicleType = results.getString("vehicle_type");
+                    String color = results.getString("color");
+                    int odometer = results.getInt("odometer");
+                    double price = results.getDouble("price");
+
+
+                    Vehicle vehicle = new Vehicle(vin, vehicleYear, make, model, vehicleType, color, odometer, price);
+                    vehicles.add(vehicle);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        return vehicles;
+    }
+
+    public List<Vehicle> getVehiclesByColor(String searchColor){
+        List<Vehicle> vehicles = new ArrayList<>();
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                     "SELECT * FROM vehicles " +
+                             "WHERE color LIKE ?")) {
+            statement.setString(1, searchColor);
+
+            try (ResultSet results = statement.executeQuery()) {
+                while (results.next()) {
+                    int vin = results.getInt("VIN");
+                    int vehicleYear = results.getInt("vehicle_year");
+                    String make = results.getString("make");
+                    String model = results.getString("model");
+                    String vehicleType = results.getString("vehicle_type");
+                    String color = results.getString("color");
+                    int odometer = results.getInt("odometer");
+                    double price = results.getDouble("price");
+
+
+                    Vehicle vehicle = new Vehicle(vin, vehicleYear, make, model, vehicleType, color, odometer, price);
+                    vehicles.add(vehicle);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        return vehicles;
+    }
+
+    public List<Vehicle> getVehiclesByMileage(int minMile, int maxMile){
+        List<Vehicle> vehicles = new ArrayList<>();
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                     "SELECT * FROM vehicles " +
+                             "WHERE odometer >= ? AND odometer <= ?")) {
+
+            statement.setInt(1, minMile);
+            statement.setInt(2, maxMile);
+
+
+            try (ResultSet results = statement.executeQuery()) {
+                while (results.next()) {
+                    int vin = results.getInt("VIN");
+                    int vehicleYear = results.getInt("vehicle_year");
+                    String make = results.getString("make");
+                    String model = results.getString("model");
+                    String vehicleType = results.getString("vehicle_type");
+                    String color = results.getString("color");
+                    int odometer = results.getInt("odometer");
+                    double price = results.getDouble("price");
+
+
+                    Vehicle vehicle = new Vehicle(vin, vehicleYear, make, model, vehicleType, color, odometer, price);
+                    vehicles.add(vehicle);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        return vehicles;
+    }
+
+    public List<Vehicle> getVehiclesByType(String searchType){
+        List<Vehicle> vehicles = new ArrayList<>();
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                     "SELECT * FROM vehicles " +
+                             "WHERE vehicle_type LIKE ?")) {
+            statement.setString(1, searchType);
+
+            try (ResultSet results = statement.executeQuery()) {
+                while (results.next()) {
+                    int vin = results.getInt("VIN");
+                    int vehicleYear = results.getInt("vehicle_year");
+                    String make = results.getString("make");
+                    String model = results.getString("model");
+                    String vehicleType = results.getString("vehicle_type");
+                    String color = results.getString("color");
+                    int odometer = results.getInt("odometer");
+                    double price = results.getDouble("price");
+
+
+                    Vehicle vehicle = new Vehicle(vin, vehicleYear, make, model, vehicleType, color, odometer, price);
+                    vehicles.add(vehicle);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        return vehicles;
     }
 
     public List<Vehicle> getAllVehicles(){
@@ -83,211 +288,6 @@ public class VehicleDAO {
         return vehicles;
     }
 
-    public List<Vehicle> getVehiclesByMileage(int minMile, int maxMile){
-        List<Vehicle> vehicles = new ArrayList<>();
-
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(
-                     "SELECT * FROM vehicles" +
-                             "WHERE odomoter <= ? AND odmometer >= ?")) {
-
-            statement.setInt(1, minMile);
-            statement.setInt(2, maxMile);
-
-
-            try (ResultSet results = statement.executeQuery()) {
-                while (results.next()) {
-                    int vin = results.getInt("VIN");
-                    int vehicleYear = results.getInt("vehicle_year");
-                    String make = results.getString("make");
-                    String model = results.getString("model");
-                    String vehicleType = results.getString("vehicle_type");
-                    String color = results.getString("color");
-                    int odometer = results.getInt("odometer");
-                    double price = results.getDouble("price");
-
-
-                    Vehicle vehicle = new Vehicle(vin, vehicleYear, make, model, vehicleType, color, odometer, price);
-                    vehicles.add(vehicle);
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-
-        return vehicles;
-    }
-
-    public List<Vehicle> getVehiclesByYear(int minYear, int maxYear){
-        List<Vehicle> vehicles = new ArrayList<>();
-
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(
-                     "SELECT * FROM vehicles" +
-                             "WHERE vehicle_year <= ? AND vehicle_year >= ?")) {
-            statement.setInt(1, minYear);
-            statement.setInt(2, maxYear);
-
-            try (ResultSet results = statement.executeQuery()) {
-                while (results.next()) {
-                    int vin = results.getInt("VIN");
-                    int vehicleYear = results.getInt("vehicle_year");
-                    String make = results.getString("make");
-                    String model = results.getString("model");
-                    String vehicleType = results.getString("vehicle_type");
-                    String color = results.getString("color");
-                    int odometer = results.getInt("odometer");
-                    double price = results.getDouble("price");
-
-
-                    Vehicle vehicle = new Vehicle(vin, vehicleYear, make, model, vehicleType, color, odometer, price);
-                    vehicles.add(vehicle);
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-
-        return vehicles;
-    }
-
-    public List<Vehicle> getVehiclesByMakeModel(String searchMake, String searchModel){
-        List<Vehicle> vehicles = new ArrayList<>();
-
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(
-                     "SELECT * FROM vehicles" +
-                             "WHERE make LIKE ? AND model LIKE ?")) {
-            statement.setString(1, searchMake);
-            statement.setString(2, searchModel);
-
-            try (ResultSet results = statement.executeQuery()) {
-                while (results.next()) {
-                    int vin = results.getInt("VIN");
-                    int vehicleYear = results.getInt("vehicle_year");
-                    String make = results.getString("make");
-                    String model = results.getString("model");
-                    String vehicleType = results.getString("vehicle_type");
-                    String color = results.getString("color");
-                    int odometer = results.getInt("odometer");
-                    double price = results.getDouble("price");
-
-
-                    Vehicle vehicle = new Vehicle(vin, vehicleYear, make, model, vehicleType, color, odometer, price);
-                    vehicles.add(vehicle);
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-
-        return vehicles;
-    }
-
-    public List<Vehicle> getVehiclesByColor(String searchColor){
-        List<Vehicle> vehicles = new ArrayList<>();
-
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(
-                     "SELECT * FROM vehicles" +
-                             "WHERE color LIKE ?")) {
-            statement.setString(1, searchColor);
-
-            try (ResultSet results = statement.executeQuery()) {
-                while (results.next()) {
-                    int vin = results.getInt("VIN");
-                    int vehicleYear = results.getInt("vehicle_year");
-                    String make = results.getString("make");
-                    String model = results.getString("model");
-                    String vehicleType = results.getString("vehicle_type");
-                    String color = results.getString("color");
-                    int odometer = results.getInt("odometer");
-                    double price = results.getDouble("price");
-
-
-                    Vehicle vehicle = new Vehicle(vin, vehicleYear, make, model, vehicleType, color, odometer, price);
-                    vehicles.add(vehicle);
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-
-        return vehicles;
-    }
-
-    public List<Vehicle> getVehiclesByType(String searchType){
-        List<Vehicle> vehicles = new ArrayList<>();
-
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(
-                     "SELECT * FROM vehicles" +
-                             "WHERE vehicle_type LIKE ?")) {
-            statement.setString(1, searchType);
-
-            try (ResultSet results = statement.executeQuery()) {
-                while (results.next()) {
-                    int vin = results.getInt("VIN");
-                    int vehicleYear = results.getInt("vehicle_year");
-                    String make = results.getString("make");
-                    String model = results.getString("model");
-                    String vehicleType = results.getString("vehicle_type");
-                    String color = results.getString("color");
-                    int odometer = results.getInt("odometer");
-                    double price = results.getDouble("price");
-
-
-                    Vehicle vehicle = new Vehicle(vin, vehicleYear, make, model, vehicleType, color, odometer, price);
-                    vehicles.add(vehicle);
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-
-        return vehicles;
-    }
-
-
-    public List<Vehicle> getVehiclesByPrice(double minPrice, double maxPrice){
-        List<Vehicle> vehicles = new ArrayList<>();
-
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(
-                     "SELECT * FROM vehicles" +
-                             "WHERE price <= ? AND price >= ?")) {
-
-            statement.setDouble(1, minPrice);
-            statement.setDouble(2, maxPrice);
-
-            try (ResultSet results = statement.executeQuery()) {
-                while (results.next()) {
-                    int vin = results.getInt("VIN");
-                    int vehicleYear = results.getInt("vehicle_year");
-                    String make = results.getString("make");
-                    String model = results.getString("model");
-                    String vehicleType = results.getString("vehicle_type");
-                    String color = results.getString("color");
-                    int odometer = results.getInt("odometer");
-                    double price = results.getDouble("price");
-
-
-                    Vehicle vehicle = new Vehicle(vin, vehicleYear, make, model, vehicleType, color, odometer, price);
-                    vehicles.add(vehicle);
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-
-        return vehicles;
-    }
 
     public boolean removeVehicle(int vin){
 
